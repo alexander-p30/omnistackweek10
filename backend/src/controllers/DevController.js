ac657d9017092b609e360be8a90751338cf561c7
estevan-alex <estevanalexandeer@gmail.com>
2020-01-16 14:36:13 -0300

    Primeiro commit, backend pronto

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,67 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+// index, show, store, update, destroy
+
+module.exports = {
+    async index (request, response) {
+        const devs = await Dev.find();
+
+        return response.json(devs);
+    },
+
+    async store (request, response) {
+        const { github_username, techs, latitude, longitude } = request.body;
+
+        let dev = await Dev.findOne( { github_username } );
+
+        if (!dev){
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+            let { name, avatar_url, bio } = apiResponse.data;
+        
+            if (!name){
+                name = apiResponse.data.login;
+            }
+        
+            const techsArray = parseStringAsArray(techs);
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            };
+        
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location,
+            });
+        
+            console.log(dev);
+        }
+        
+        return response.json(dev);
+    },
+
+    async update(request, response){
+        const { github_username, name, bio, techs } = request.body;
+        console.log(request.body);
+
+        let dev = await Dev.findOne( { github_username } );
+        console.log(dev);
+
+        dev = await dev.update({
+            name: request.body.name,
+            bio: request.body.bio,
+            techs: parseStringAsArray(request.body.techs)
+        });
+
+        dev = await Dev.findOne( { github_username } );
+
+        return response.json(dev);
+    }
+};
---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://estevan:dbpassword@cluster0-cvcev.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+// get, post, put, delete
+
+//Tipos de parâmetros
+// Query Params: request.query (req.query) (Filtros, ordenação, paginação, etc...)
+// Route Params: request.params (req.params) (Identificar um recurso para alteração ou remoção.)
+// Body: request.body (req.body) (Dados para criação ou alteração de registro)
+
+//MongoDB (Não-relacional)
+
+app.listen(3000);
+
